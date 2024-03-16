449e6de61682366ddda459523bf2475c4a3aa994
AnnaIossifov <iossifov.anna@gmail.com>
2024-03-16 20:56:18 +0200

    Add the html, css,js files as needed

---
+++ js/meme.controller.js
@@ -0,0 +1,29 @@
+
+
+function loadMemes(){
+
+}
+
+function onCreateMeme(imageUrl, text){
+
+}
+
+function onUpdateMeme(memeId, newText){
+
+}
+
+function onDeleteMeme(memeId){
+
+}
+
+function initEventListeners(){
+
+}
+
+function onToggleModal(){
+
+}
+
+function showErrorMessage(message){
+
+}
